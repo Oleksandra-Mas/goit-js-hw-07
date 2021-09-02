b0c7874e9df55ec3258f71f5b57126b3c2ebf2a2
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-09-02 21:51:05 +0300

    did 4th task

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const refs = {
+    decrementRef: document.querySelector('[data-action="decrement"]'),
+    incrementRef: document.querySelector('[data-action="increment"]'),
+    valueRef: document.querySelector('#value'),
+};
+let counterValue = 0;
+const changeValue  =function () {
+    refs.valueRef.textContent = counterValue;
+}
+const increment  =function () {
+    counterValue += 1;
+    changeValue();
+}
+const decrement   =function () {
+    counterValue -= 1;
+    changeValue();
+}
+refs.decrementRef.addEventListener('click', decrement);
+refs.incrementRef.addEventListener('click', increment);
