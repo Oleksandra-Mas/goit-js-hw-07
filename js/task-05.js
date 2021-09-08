7df34e794fb72e097ff488219b5031c4c80ac95c
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-09-08 23:37:55 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,8 @@ const refs = {
     outputRef: document.querySelector('span#name-output'),
 };
 const inputValueHandle = function () {
-    refs.outputRef.textContent = refs.inputRef.value
-        ? refs.inputRef.value
+    refs.outputRef.textContent = refs.inputRef.value.trim()
+        ? refs.inputRef.value.trim()
         : 'незнакомец';
 };
 refs.inputRef.addEventListener('input', inputValueHandle);
